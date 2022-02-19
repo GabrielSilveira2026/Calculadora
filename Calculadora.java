4d8e7ebe9146de45a0577463b44f6d95a025ad72
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2022-02-18 21:47:16 -0300

    Função subtração criada

--- Calculadora.java
+++ Calculadora.java
@@ -12,6 +12,11 @@ public class Calculadora{
         return resultSoma;
     }
 
+    public int subtrair(int valor1, int valor2){
+        this.resultSub = valor1 - valor2;
+        return resultSub;
+    }
+
     @Override
     public String toString() {
         return "\nSoma = " + resultSoma +
--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -4,6 +4,7 @@ public class TesteCalculadora {
         Calculadora e1 = new Calculadora();
 
         e1.somar(2,2);
+        e1.subtrair(9,2);
         System.out.println(e1);
     }
 }
